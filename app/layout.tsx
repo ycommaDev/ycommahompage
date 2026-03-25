8012c8e2c4212a590627478e4db57d51c078b43e
ycommaDev <toktokhan@ycomma.com>
2026-03-25 19:56:12 +0900

    Revert "Update layout.tsx"
    This reverts commit ab9b834b84f82e51fb8737447cf1f01cdbb25f77.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-image.svg",
         width: 1200,
         height: 630,
         alt: "YCOMMA Open Graph Image",
