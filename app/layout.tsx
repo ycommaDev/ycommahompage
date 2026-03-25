ab9b834b84f82e51fb8737447cf1f01cdbb25f77
ycommaDev <toktokhan@ycomma.com>
2026-03-25 19:43:42 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.svg",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "YCOMMA Open Graph Image",
